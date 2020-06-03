870ec6b458a16df575671cbd58f6b3d9651eff7e
drew ogryzek <drew@vancouvertechpodcast.ca>
2020-06-02 21:00:28 -0700

    Adds setup for exercise 2

--- day_05/users/app.py
+++ day_05/users/app.py
@@ -11,10 +11,44 @@ def save(name):
 
 def show_names():
     names = open('names', 'r')
-    for name in names:
-        print(name, end='')
+    names_list = names.readlines()
+    for name in names_list:
+        print("{} {}".format(names_list.index(name), name), end='')
 
-name = get_user_name()
-save(name)
-show_names()
+def update_name():
+    print("TODO: Implement me!")
 
+def remove_name(name_index):
+    print("TODO: Implement me!")
+
+options = '''
+NAMES
+=====
+
+Help Menu
+
+q           quit
+v           view names
+a           add name
+e           edit name
+r           remove name
+'''
+
+def get_user_input():
+    return input("Enter an option: ").lower()
+
+while True:
+    print(options)
+    user_input = get_user_input()
+    if user_input == "q":
+        break
+    elif user_input == "v":
+        show_names()
+    elif user_input == "a":
+        name = get_user_name()
+        save(name)
+    elif user_input == "e":
+        update_name()
+    elif user_input == "r":
+        name_index = int(input("Enter the number of the name to remove: "))
+        remove_name(name_index)
