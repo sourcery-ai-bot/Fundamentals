f4c16ad4df25a03aae87b4400b17a6c12911b5b8
drew ogryzek <drew@vancouvertechpodcast.ca>
2020-06-04 20:34:49 -0700

    Adds day 6 exercise 02

--- day_06/testing/app.py
+++ day_06/testing/app.py
@@ -2,4 +2,15 @@
 
 # 1: write a funtion that takes a list as an argument, and returns the list reversed.
 def reverse_list(list):
-    return ['d', 'c', 'b', 'a']
+    reversed_list = []
+    while len(list) > 0:        
+        reversed_list.append(list.pop())
+
+    return reversed_list
+
+# 2: write a function that takes a country, and two lists (cities and populations) as arugments.
+# it should return a dictionary whose key => value mappings are country => city. Cities should also
+# be dictionaries of city => population.
+def city_pop_dict(country, cities, populations):
+    return_dictionary = {}
+    return return_dictionary
--- day_06/testing/tests.py
+++ day_06/testing/tests.py
@@ -1,4 +1,4 @@
-from app import reverse_list
+from app import reverse_list, city_pop_dict
 
 def test_reverse_list_01():
     test_list = ['a', 'b', 'c', 'd']
@@ -24,9 +24,45 @@ def test_reverse_list_02():
         print("Expected: {}".format(expected))
         print("Got: {}".format(actual))
 
+def test_reverse_list_03():
+    test_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+    expected = [0, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+    actual = reverse_list(test_list)
+
+    if actual == expected:
+        print("test reverse list passed!")
+    else:
+        print("FAILED: test reverse list failed.")
+        print("Expected: {}".format(expected))
+        print("Got: {}".format(actual))
+
+def test_city_pop_dict_01():
+    country = "Russia"
+    cities = ["Moscow", "Saint Petersburg", "Novosibirsk"]
+    populations = [12432531, 5383890, 1618039]
+
+    expected = {
+        "Russia": {
+            "Moscow": 12432531,
+            "Saint Petersburg": 5383890,
+            "Novosibirsk": 1618039
+        }
+    }
+
+    actual = city_pop_dict(country, cities, populations)
+
+    if actual == expected:
+        print("test reverse list passed!")
+    else:
+        print("FAILED: test reverse list failed.")
+        print("Expected: {}".format(expected))
+        print("Got: {}".format(actual))
+
 
 def run_tests():
     test_reverse_list_01()
     test_reverse_list_02()
+    test_reverse_list_03()
+    test_city_pop_dict_01()
 
 run_tests()
