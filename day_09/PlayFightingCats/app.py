98a69b5e27838ddb7c4bfa03cb2b5cf8698faad7
drew ogryzek <drew@vancouvertechpodcast.ca>
2020-06-18 18:24:45 -0700

    Clean up playfight and app

--- day_09/PlayFightingCats/app.py
+++ day_09/PlayFightingCats/app.py
@@ -8,22 +8,12 @@ from food import Food
 from cat import Cat
 from competition import Competition
 
-
 # Create some food
 prize_food = Food()
-print("TheFood energy_value is: {}".format(prize_food.energy_value))
 
 # Create two cats that will playfight
 sunny = Cat('Sunny the Cat', 16, 27, 'medium')
 bob = Cat('Robert the Cat', 9, 30, 'large')
-
-sunny.string()
-bob.string()
-
-sunny.eat(prize_food)
-bob.eat(prize_food)
-
-
 
 # we have two cats, sunny and bob, who are competing in a 
 # playfight for TheFood.
@@ -35,7 +25,5 @@ bob.eat(prize_food)
 # based on those attributes.
 
 # What should those rules be?
-
 competition1 = Competition(sunny, bob)
-competition1.playfight()
-
+competition1.playfight(prize_food)
--- day_09/PlayFightingCats/competition.py
+++ day_09/PlayFightingCats/competition.py
@@ -17,12 +17,25 @@ class Competition:
         chance_to_win += cat.pride
         return chance_to_win
 
-    def playfight(self):
+    def playfight(self, prize):
+        print("Starting playfight between {} and {}".format(self.cat_1.name, self.cat_2.name))
+        self.cat_status()
         cat_1_chance_to_win = self.determine_chance_to_win(self.cat_1)
         cat_2_chance_to_win = self.determine_chance_to_win(self.cat_2)
+        self.cat_1.energy -= 2
+        self.cat_2.energy -= 2
         if cat_1_chance_to_win == cat_2_chance_to_win:
             print("It was a tie!")
         elif cat_1_chance_to_win > cat_2_chance_to_win:
             print("{} wins!".format(self.cat_1.name))
+            self.cat_1.eat(prize)
+            self.cat_1.pride += 1
         else:
             print("{} wins!".format(self.cat_2.name))
+            self.cat_2.eat(prize)
+            self.cat_2.pride += 1
+        self.cat_status()
+        
+    def cat_status(self):
+        self.cat_1.string()
+        self.cat_2.string()
