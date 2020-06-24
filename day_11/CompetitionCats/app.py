b5ab707a34cf62d252974e48640da2f3e0631f0d
drew ogryzek <drew@vancouvertechpodcast.ca>
2020-06-23 22:54:51 -0700

    Adds playfight to CompetitionCats CLI

--- day_11/CompetitionCats/app.py
+++ day_11/CompetitionCats/app.py
@@ -13,11 +13,53 @@ def new_cat(name):
     size = random.choice(sizes)
     cat = Cat(name, pride, energy, size)
     cat.save()
+    return cat
 
 def find_cats():
     print("Finding a cat!")
     cats = Cat.get_all()
     return cats
+
+def find_or_create(cat_name):
+    cat = Cat.find_by_name(cat_name)
+    if cat == '':
+        return new_cat(cat_name)
+    else:
+        return cat
+
+def determine_chance_to_win(cat):
+    chance_to_win = 0
+
+    if cat.size == 'medium':
+        chance_to_win += 5
+    elif cat.size == 'large':
+        chance_to_win += 7
+    else:
+        chance_to_win += 3
+
+    print("Cat Name: {}, Energy: {}".format(cat.name, type(cat.energy)))
+    chance_to_win += cat.energy
+    chance_to_win += cat.pride
+    return chance_to_win
+
+def playfight(comp, player):
+    comp_chance = determine_chance_to_win(comp)
+    player_chance = determine_chance_to_win(player)
+    comp.energy -= 2
+    player.energy -= 2
+    if comp_chance == player_chance:
+        print("It was a tie!")
+    elif comp_chance > player_chance:
+        print("{} wins!".format(comp.name))
+        comp.energy += 3
+        comp.pride += 1
+    else:
+        print("{} wins!".format(player.name))
+        player.energy += 3
+        player.pride += 1
+    print("Here is the status of your cat:")
+    print("Energy: {}, Pride: {}".format(player.energy, player.pride))
+
 
 menu = '''
 CompetitionCats!
@@ -25,14 +67,18 @@ CompetitionCats!
 Menu
 n  make a new cat
 u  use an existing cat
+p  playfight
 q  quit
 
 What would you like to do?
 '''
 
 def run():
+    global player_1
+    global comp_1
     while True:
         user_input = input(menu)
+        
         if user_input == 'n':
             name = input("What is your cat's name? ")
             new_cat(name)
@@ -43,8 +89,18 @@ def run():
                 print("{} was not found. Please select from the list: ".format(cat_name))
                 cat_name = input("which cat would you like to use?\n{}\n".format(cats))
 
-            cat = Cat.find_by_name(cat_name)
-            print("You've chosen {}".format(cat.name))
+            player_1 = Cat.find_by_name(cat_name)
+            print("You've chosen {}".format(player_1.name))
+        elif user_input == 'p':
+            if player_1 == '':
+                print("please select a cat first.")
+                pass
+            else:
+                comp_1 = find_or_create('ComputerCat')
+                print("Your cat is {}".format(player_1.name))
+                print("Your opponent is {}".format(comp_1.name))
+                playfight(comp_1, player_1)
+
         elif user_input == 'q':
             break
         else:
--- day_11/CompetitionCats/cat.py
+++ day_11/CompetitionCats/cat.py
@@ -34,9 +34,9 @@ class Cat:
         for cat in cats_db.readlines():
             # "Sheera, 20, 18, small\n"
             cat = cat.strip('\n').split(',')
-            # ["Sheera", 20, 18, "small"]
+            # ["Sheera", "20", "18", "small"]
             if cat[0] == name:
-                found_cat = cls(cat[0], cat[1], cat[2], cat[3])
+                found_cat = cls(cat[0], int(cat[1]), int(cat[2]), cat[3])
                 print("found cat: {}, {}".format(found_cat.name, found_cat.energy))
             
         return found_cat
