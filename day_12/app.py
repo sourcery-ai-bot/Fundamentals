0a617aa2055b6424745330c570d925d0ac8fcd32
drew ogryzek <drew@vancouvertechpodcast.ca>
2020-06-25 19:19:25 -0700

    Adds answer to Exercise 02 a

--- day_12/app.py
+++ day_12/app.py
@@ -7,5 +7,10 @@ def index():
     if request.method == 'GET':
         return render_template('index.html')
     elif request.method == 'POST':
-        return render_template('greet.html', name = request.form['name'])
+        name = request.form['name']
+        email = request.form['email']
+        if len(name) < 2 or len(email) < 5:
+            return render_template('index.html')
+        else:
+            return render_template('greet.html', name = name)
 app.run()
---
+++ day_12/play.py
@@ -0,0 +1,12 @@
+
+email = ""
+if len(email) < 5:
+    print("invalid email")
+
+result = email.split('@')
+print(len(result))
+
+"something"
+"lol.com"
+"something@something.something"
+"a@a.c"
