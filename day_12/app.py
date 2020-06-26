2d1988d13193b24c06906f204f3fab3653ef90d3
drew ogryzek <drew@vancouvertechpodcast.ca>
2020-06-25 18:45:12 -0700

    Adds answers to Module 12, exercise 01

--- day_12/app.py
+++ day_12/app.py
@@ -7,6 +7,5 @@ def index():
     if request.method == 'GET':
         return render_template('index.html')
     elif request.method == 'POST':
-        return 'Hello!'
-
+        return render_template('greet.html', name = request.form['name'])
 app.run()
